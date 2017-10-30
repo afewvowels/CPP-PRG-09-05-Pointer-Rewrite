ee17da592a2dbf219cadc63591903908cfffac2c
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-30 13:41:08 -0400

    I think this is what the prompt describes. Needs comments.

--- CPP-PRG-09-05-Pointer-Rewrite/main.cpp
+++ CPP-PRG-09-05-Pointer-Rewrite/main.cpp
@@ -5,11 +5,41 @@
 //  Created by Keith Smith on 10/30/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  The following function uses reference variables as parameters. Rewrite the function
+//  so it uses pointers instead of reference variables, then demonstrate the function in a
+//  complete program.
+//
+//  int doSomething(int &x, int &y)
+//  {
+//      int temp = x;
+//      x = y * 10;
+//      y = temp * 10;
+//      return x + y;
+//  }
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int doSomething(int *, int *);
+
+int main()
+{
+    int intVal1 = 5;
+    int intVal2 = 10;
+    int intVal3;
+    
+    intVal3 = doSomething(&intVal1, &intVal2);
+    
+    cout << intVal3 << endl;
+    
     return 0;
+}
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
 }
